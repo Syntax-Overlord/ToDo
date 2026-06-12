e79317605c8b6a935c915d905af056038fcf01e2
Syntax-Overlord <laksh2997@gmail.com>
2026-06-12 23:38:36 +0530

    Refactor docstrings in Credential class for clarity and completeness

--- src/credentials.py
+++ src/credentials.py
@@ -4,13 +4,23 @@ import sqlite3
 class Credential:
 
     def __init__(self):
-        """TODO"""
+        """
+        Connects with credentials.db and creates cursor object.
+        """
+
         self.conn = sqlite3.connect("../data/credentials.db")
         self.conn.row_factory = sqlite3.Row
         self.cursor = self.conn.cursor()
         self.table_name = self.table()
 
     def verify_user_id(self, username: str, password: str) -> bool:
+        """
+        Accepts username and password as arguments.
+
+        Returns True if credentials are correct or if account doesn't exist (creates account).
+        Returns False if credentials are incorrect.
+        """
+
         username = username.lower().strip()
 
         # 1. Check if user exists
@@ -46,13 +56,20 @@ class Credential:
         return False
 
     def table(self) -> str:
+        """
+        Connects with table (creates it if it doesn't exist).
+        """
+
         self.cursor.execute("""CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT
-            username TEXT
-            password TEXT
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT,
+            password_hash TEXT
             );""")
         self.conn.commit()
         return "users"
 
     def close(self) -> None:
+        """
+        Closes the sql connection.
+        """
         self.conn.close()
